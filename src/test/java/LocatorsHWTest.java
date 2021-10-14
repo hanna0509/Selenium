f2e3011e0410d39fe67252c3baaf20b7d5011558
Hanna <kravtsova.hanna.v@gmail.com>
2021-10-14 22:30:44 +0300

    reformat file

--- src/test/java/LocatorsHWTest.java
+++ src/test/java/LocatorsHWTest.java
@@ -13,23 +13,29 @@ public class LocatorsHWTest {
 
     @Test
 
-    public void LocatorsHWTest() {
+    public void locatorsHWTest() {
         System.setProperty("webdriver.chrome.driver", "chromedriver1.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amway.ua/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
         WebElement LoginButton = driver.findElement(By.cssSelector("#amweia-app > div.page > header > div > div > div.user-area.user-area--login > div.user-area-login-links.user-area-login-links--desktop > a.user-area-login-links__link.user-area-login-links__link--signin.js-sign-in"));
         LoginButton.click();
+
         WebElement LoginTab = driver.findElement(By.xpath("/html/body/div/div[2]/div/app-root/div/div[1]/div/app-signin/div/div/div/div/div/div/app-id-toggle/div/div/div[1]/label[2]"));
         LoginTab.click();
+
         WebElement LoginField = driver.findElement(By.id("username"));
         LoginField.sendKeys("TestUser");
+
         WebElement PasswordField = driver.findElement(By.id("password"));
         PasswordField.click();
         PasswordField.sendKeys("password1");
+
         WebElement SubmitButton = driver.findElement(By.xpath("/html/body/div/div[2]/div/app-root/div/div[1]/div/app-signin/div/div/div/div/div/div/app-button/button"));
         SubmitButton.click();
+
         var waiter = new WebDriverWait(driver, 10);
         waiter.until(new ExpectedCondition<Boolean>() {
             @NullableDecl
